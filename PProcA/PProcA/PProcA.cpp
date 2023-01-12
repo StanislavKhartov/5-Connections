471ec79a21d083b51e6b7fe39ec8fd472e64a5a4
Stanislav <89999222+StanislavKhartov@users.noreply.github.com>
2023-01-12 17:58:44 +0300

    Add files via upload

---
+++ PProcA/PProcA/PProcA.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+#include <stdio.h>
+#include <tchar.h>
+
+void callMenu() {
+    std::cout << "1 - Send message\n";
+    std::cout << "2 - Recieve message\n";
+    std::cout << "0 - Exit\n";
+}
+
+int main(int argc, char** args)
+{
+    HANDLE hPipeOut = (HANDLE)std::stoull(args[1]);
+    HANDLE hPipeIn = (HANDLE)std::stoull(args[2]);
+    HANDLE hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hConsoleIn = GetStdHandle(STD_INPUT_HANDLE);
+    int state = 1;
+    while (state) {
+        callMenu();
+        std::cin >> state;
+        if (state > 2 || state < 0) {
+            std::cout << "Wrong state";
+            return 1;
+        }
+        if (state == 1) {
+            std::wstring input;
+            std::wcin >> input;
+            _TCHAR line[128];
+            wcscpy_s(line, input.c_str());
+            WriteFile(hPipeOut, line, input.size() * sizeof(_TCHAR), NULL, NULL);
+        }
+        if (state == 2) {
+            DWORD bytesAvail = 0;
+            if (!PeekNamedPipe(hPipeIn, NULL, 0, NULL, &bytesAvail, NULL)) {
+                std::cout << "Failed to call PeekNamedPipe" << std::endl;
+            }
+            if (bytesAvail) {
+                _TCHAR buf[512];
+                DWORD n;
+                BOOL success = ReadFile(hPipeIn, buf, bytesAvail * sizeof(_TCHAR), &n, NULL);
+                if (!success || n == 0) {
+                    std::cout << "Failed to call ReadFile" << std::endl;
+                }
+                std::wcout << std::wstring(buf, buf + n);
+                std::cout << '\n';
+            }
+            else {
+                std::cout << "Nothing to receive" << '\n';
+            }
+        }
+    }
+}
---
+++ PipeStartAPP/PipeStartAPP/PipeStartAPP.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <string>
+#include "stdafx.h"
+
+int main()
+{
+	_TCHAR streamErr[100];
+	_TCHAR streamIn0[100];
+	_TCHAR streamOut0[100];
+	_TCHAR streamIn1[100];
+	_TCHAR streamOut1[100];
+
+	HANDLE hWritePipe0, hReadPipe0;
+	SECURITY_ATTRIBUTES saPipe0 = { sizeof(SECURITY_ATTRIBUTES),NULL,TRUE };
+	SECURITY_ATTRIBUTES saFileIn0 = { sizeof(SECURITY_ATTRIBUTES),NULL,TRUE };
+	HANDLE hFileIn0 = CreateFile(L"In0.txt", GENERIC_READ, 0,
+		&saFileIn0,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (hFileIn0 == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"Could not open file <%s> (error %d)\n", streamIn0, GetLastError());
+		getchar();
+		return 0;
+	}
+
+	SECURITY_ATTRIBUTES saFileErr = { sizeof(SECURITY_ATTRIBUTES),nullptr,TRUE };
+	HANDLE hFileErr = CreateFile(L"Errors.txt", GENERIC_WRITE, 0,
+		&saFileErr,
+		CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+	if (hFileErr == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"Could not open file <%s> (error %d)\n", streamErr, GetLastError());
+		getchar();
+		return 0;
+	}
+
+	SECURITY_ATTRIBUTES saFileOut0 = { sizeof(SECURITY_ATTRIBUTES),nullptr,TRUE };
+	HANDLE hFileOut0 = CreateFile(L"Out0.txt", GENERIC_WRITE, 0,
+		&saFileOut0,
+		CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (hFileOut0 == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"Could not open file <%s> (error %d)\n", streamOut0, GetLastError());
+		getchar();
+		return 0;
+	}
+
+	BOOL bPipe0 = CreatePipe(&hReadPipe0, &hWritePipe0, &saPipe0, 0);
+	if (!bPipe0)
+	{
+		wprintf(L"Could not create Pipe (error %d)\n", GetLastError());
+		getchar();
+		return 0;
+	}
+
+	HANDLE hWritePipe1, hReadPipe1;
+	SECURITY_ATTRIBUTES saPipe1 = { sizeof(SECURITY_ATTRIBUTES),NULL,TRUE };
+	SECURITY_ATTRIBUTES saFileIn1 = { sizeof(SECURITY_ATTRIBUTES),NULL,TRUE };
+	HANDLE hFileIn1 = CreateFile(L"In1.txt", GENERIC_READ, 0,
+		&saFileIn1,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (hFileIn1 == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"Could not open file <%s> (error %d)\n", streamIn0, GetLastError());
+		getchar();
+		return 0;
+	}
+
+	SECURITY_ATTRIBUTES saFileOut1 = { sizeof(SECURITY_ATTRIBUTES),NULL,TRUE };
+	HANDLE hFileOut1 = CreateFile(L"Out1.txt", GENERIC_WRITE, 0,
+		&saFileOut1,
+		CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (hFileOut1 == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"Could not open file <%s> (error %d)\n", streamOut0, GetLastError());
+		getchar();
+		return 0;
+	}
+
+	BOOL bPipe1 = CreatePipe(&hReadPipe1, &hWritePipe1, &saPipe1, 0);
+	if (!bPipe1)
+	{
+		wprintf(L"Could not create Pipe (error %d)\n", GetLastError());
+		getchar();
+		return 0;
+	}
+
+	unsigned long long writeHandle0 = (unsigned long long)hWritePipe0;
+	unsigned long long readHandle0 = (unsigned long long)hReadPipe1;
+	unsigned long long writeHandle1 = (unsigned long long)hWritePipe1;
+	unsigned long long readHandle1 = (unsigned long long)hReadPipe0;
+
+	STARTUPINFO startupInfo = { sizeof startupInfo };
+	PROCESS_INFORMATION processInformation = { 0 };
+
+	std::wstring args0 = L"..\\..\\PProcA\\x64\\Debug\\PProcA.exe " + std::to_wstring(writeHandle0) + L" " + std::to_wstring(readHandle0);
+	_TCHAR line0[512];
+	wcscpy_s(line0, args0.c_str());
+
+	BOOL bPr0 = CreateProcess(NULL,
+		line0, NULL,
+		NULL, TRUE,
+		CREATE_NEW_CONSOLE,
+		NULL,
+		NULL,
+		&startupInfo,
+		&processInformation);
+	if (!bPr0)
+	{
+		wprintf(L"Could not create process <%s>(error %d)\n", GetLastError());
+		getchar();
+		return 0;
+	}
+
+	std::wstring args1 = L"..\\..\\PProcA\\x64\\Debug\\PProcA.exe " + std::to_wstring(writeHandle1) + L" " + std::to_wstring(readHandle1);
+	_TCHAR line[512];
+	wcscpy_s(line, args1.c_str());
+	BOOL bPr1 = CreateProcess(NULL,
+		line, NULL,
+		NULL, TRUE,
+		CREATE_NEW_CONSOLE,
+		NULL,
+		NULL,
+		&startupInfo,
+		&processInformation);
+	if (!bPr1)
+	{
+		wprintf(L"Could not create process <%s>(error %d)\n", GetLastError());
+		getchar();
+		return 0;
+	}
+
+
+	CloseHandle(hWritePipe1);
+	CloseHandle(hReadPipe0);
+	CloseHandle(hWritePipe0);
+	CloseHandle(hReadPipe1);
+	CloseHandle(hFileErr);
+	return 0;
+}
