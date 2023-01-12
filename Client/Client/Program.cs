5677899dcc723d794ead486d94d1dce0bc3990b2
Stanislav <fpm.hartovSV@bsu.by>
2023-01-12 18:58:18 +0300

    Add solution

---
+++ Client/Client/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using String = System.String;
+
+// Client app is the one sending messages to a Server/listener.
+// Both listener and client can send messages back and forth once a
+// communication is established.
+public class SocketClient
+{
+    public static int Main(String[] args)
+    {
+        StartClient();
+        return 0;
+    }
+
+    public static void StartClient()
+    {
+        void callMenu()
+        {
+            Console.WriteLine("1 - Send message\n");
+            Console.WriteLine("2 - Recieve message\n");
+            Console.WriteLine("0 - Exit\n");
+        }
+        byte[] bytes = new byte[1024];
+
+        try
+        {
+            // Connect to a Remote server
+            // Get Host IP Address that is used to establish a connection
+            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+            // If a host has multiple addresses, you will get a list of addresses
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP  socket.
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try
+            {
+                // Connect to Remote EndPoint
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                
+                // Encode the data string into a byte array.
+                byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                // Send the data through the socket.
+                int bytesSent = sender.Send(msg);
+
+                // Receive the response from the remote device.
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("Echoed test = {0}",
+                    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                // Release the socket.
+                int state = 1;
+                while (state != 0)
+                {
+                    callMenu();
+                    state = Convert.ToInt16(Console.ReadLine());
+                    if (state > 2 || state < 0)
+                    {
+                        Console.WriteLine("Wrong state");
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+                    }
+                    if (state == 1)
+                    {
+                        int num = Convert.ToInt16(Console.ReadLine());
+                        byte[] msg1 = Encoding.ASCII.GetBytes(num.ToString());
+                        sender.Send(msg1);
+
+                    }
+                    if (state == 2)
+                    {
+                        int bytesRec1 = sender.Receive(bytes);
+                        Console.WriteLine("recieved data = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec1));
+                    }
+                }
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// Socket Listener acts as a server and listens to the incoming
+// messages on the specified port and protocol.
+public class SocketListener
+{
+    public static int Main(String[] args)
+    {
+        StartServer();
+        return 0;
+    }
+
+  
+    public static void StartServer()
+    {
+        // Get Host IP Address that is used to establish a connection
+        // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+        // If a host has multiple addresses, you will get a list of addresses
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        void callMenu()
+        {
+            Console.WriteLine("1 - Send message\n");
+            Console.WriteLine("2 - Recieve message\n");
+            Console.WriteLine("0 - Exit\n");
+        }
+
+        try
+        {
+
+            // Create a Socket that will use Tcp protocol
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            // A Socket must be associated with an endpoint using the Bind method
+            listener.Bind(localEndPoint);
+            // Specify how many requests a Socket can listen before it gives Server busy response.
+            // We will listen 10 requests at a time
+            listener.Listen(10);
+
+            Console.WriteLine("Waiting for a connection...");
+            Socket handler = listener.Accept();
+
+            // Incoming data from the client.
+            string data = null;
+            byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                if (data.IndexOf("<EOF>") > -1)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Text received : {0}", data);
+
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            handler.Send(msg);
+            int state = 1;
+            while (state != 0)
+            {
+                callMenu();
+                state = Convert.ToInt16(Console.ReadLine());
+                if (state > 2 || state < 0)
+                {
+                    Console.WriteLine("Wrong state");
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+                if (state == 1)
+                {
+                    int num = Convert.ToInt16(Console.ReadLine());
+                    byte[] msg1 = Encoding.ASCII.GetBytes(num.ToString());
+                    handler.Send(msg1);
+
+                }
+                if (state == 2)
+                {
+                    int bytesRec = handler.Receive(bytes);
+                    string data1 = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    Console.WriteLine("Text received : {0}", data1);
+                }
+            }
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\n Press any key to continue...");
+        Console.ReadKey();
+    }
+}
